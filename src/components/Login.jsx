aa636ca4cadd89b6a7739682ceafa5eaf01e605e
Md Arifur Rahman <sojibkhanarif999@gmail.com>
2023-04-21 12:47:21 +0600

    sweet alert add

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,41 @@
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import React from 'react';
+import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useRef } from 'react';
 import { useState } from 'react';
 import app from '../firebase/firebase.init';
 import { Link } from 'react-router-dom';
+import swal from 'sweetalert';
+import Button from 'react-bootstrap/Button';
 
 const auth = getAuth(app);
 const Login = () => {
 
     const [error, setError] = useState('');
     const [success, setSuccess] = useState('')
+    const emailRef = useRef();
 
+    //Sweet alert function
+    function handleButtonClick() {
+        swal({
+            title: "Error-Login",
+            text: "User not register !!! You are new to website baby !!!",
+            icon: "error",
+            button: "OK"
+        });
+    }
+    //show password
+
+    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const toggleShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    ////finish
 
     const handleSubmission = (event) => {
         setSuccess('');
@@ -47,9 +73,9 @@ const Login = () => {
             .then(result => {
                 const loggedUser = result.user;
                 console.log(loggedUser);
-                if (!loggedUser.emailVerified) {
-                    alert("how are you")
-                }
+                // if (!result.user.emailVerified) {
+                //     alert("how are you")
+                // }
                 // event.target.reset();
                 setSuccess('Successfully LogIn');
                 setError('')
@@ -57,8 +83,35 @@ const Login = () => {
             })
             .catch(error => {
                 // console.log(error.code);
+                // If the user is not registered, show an alert
+                if (error.code === 'auth/user-not-found') {
+                    // alert('User not registered.');
+                    handleButtonClick();
+                    setError('User not registered.')
+                } else {
+                    alert(error.message);
+                }
                 console.log(error.meassage);
                 setError(error.meassage)
+            })
+
+
+    }
+
+    const handleResetPassword = (event) => {
+        const email = emailRef.current.value;
+        console.log(emailRef.current.value);
+        if (!email) {
+            alert("Please enter your email to Reset Password");
+            return;
+        }
+        sendPasswordResetEmail(auth, email)
+            .then(() => {
+                alert("please check your email")
+            })
+            .catch(error => {
+                console.log(error.code);
+                console.log(error.message);
             })
 
 
@@ -70,21 +123,31 @@ const Login = () => {
             <form onSubmit={handleSubmission} className="mx-auto col-md-6">
                 <div className="form-group">
                     <label htmlFor="email">Email address</label>
-                    <input type="email" className="form-control" id="email" name='email' placeholder="Enter email" />
+                    <input type="email" className="form-control" id="email" name='email' ref={emailRef} placeholder="Enter email" />
                 </div>
                 <div className="form-group">
+
                     <label htmlFor="password">Password</label>
-                    <input type="password" className="form-control" id="password" name='password' placeholder="Password" />
+                    <input type={showPassword ? "text" : "password"} value={password} onChange={handlePasswordChange} className="form-control" id="password" name='password' placeholder="Password" />
+                    <button className='rounded-2 my-2 border border-primary' onClick={toggleShowPassword}>{showPassword ? "Hide Password" : "Show Password"}</button>
+                    {/* <label htmlFor="password">Password</label>
+                    <input type="password" className="form-control" id="password" name='password' placeholder="Password" /> */}
                 </div>
                 <div className="form-check">
                     <input type="checkbox" className="form-check-input" id="rememberMe" />
                     <label className="form-check-label" htmlFor="rememberMe">Remember me</label>
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
+                <p className='mt-4' ><small>forget your password? Please <button onClick={handleResetPassword} className='btn btn-link'>Reset</button>  now !</small></p>
+
                 <p className='mt-4' ><small>Are you new to this website? Please <Link to='/resistor'>Resister</Link>  now !</small></p>
+
                 <p className='text-center text-danger fw-bold'>{error}</p>
                 <p className='text-center text-success fw-bold'>{success}</p>
             </form>
+            <Button variant="primary" onClick={handleButtonClick}>
+                Show Alert
+            </Button>
         </div>
     );
 };
